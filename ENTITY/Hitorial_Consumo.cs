225886189c6a0cd16693303068650a1e3bb42db3
Andres-alfons <andreselcrack1022@gmail.com>
2025-10-09 17:19:51 -0500

    Agregar archivos de proyecto.

---
+++ ENTITY/Hitorial_Consumo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY
+{
+    public class Hitorial_Consumo
+    {
+        public Hitorial_Consumo() { }
+        
+        
+        public DateTime Fecha_Consumo { get; set; }
+        public float cantidad_agua { get; set; }
+        public float cantidad_energia { get; set; }
+            
+    }
+}
